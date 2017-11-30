c10fe65675ac97374d102933e4058ae35e18a97d
Adi Bhatia <arbhatia@scu.edu>
2017-11-29 20:29:47 -0800

    updated file
    added switch statements

--- project/procSetWIPCore.php
+++ project/procSetWIPCore.php
@@ -36,13 +36,71 @@ function insertWIPIntoDB($StudentID,$highlights)
     exit;
   }
    
-   $query = oci_parse($conn, "Insert Into Book_HighLights(title,highlight) values(upper(:title),:highlight)");
+   $query = oci_parse($conn, "Insert Into CoreRequirements(StudentID,highlight) values(upper(:title),:)");
 
    // $highlights is an array containing all the values
    // selected from the multiple checkboxes
 
     foreach ( $highlights as $oneHighlight ) 
     {
+      switch ($oneHighlight) {
+        case 'CTW1':
+            $oneHighlight = 'wip';
+            break;
+        case 'CTW2':
+            $oneHighlight = 'wip';
+            break;
+        case 'CNI1':
+            $oneHighlight = 'wip';
+            break;
+        case 'CNI2':
+            $oneHighlight = 'wip';
+            break;
+        case 'CNI3':
+            $oneHighlight = 'wip';
+            break;
+        case 'STS':
+            $oneHighlight = 'wip';
+            break;
+        case 'CivicEngagement':
+            $oneHighlight = 'wip';
+            break;
+        case 'Diversity':
+            $oneHighlight = 'wip';
+            break;
+        case 'Ethics':
+            $oneHighlight = 'wip';
+            break;
+        case 'RTC1':
+            $oneHighlight = 'wip';
+            break;
+        case 'RTC2':
+            $oneHighlight = 'wip';
+            break;
+        case 'RTC3':
+            $oneHighlight = 'wip';
+            break;
+        case 'ELSJ':
+            $oneHighlight = 'wip';
+            break;
+        case 'AdvancedWriting':
+            $oneHighlight = 'wip';
+            break;
+        case 'Language':
+            $oneHighlight = 'wip';
+            break;
+        case 'NaturalScience':
+            $oneHighlight = 'wip';
+            break;
+        case 'SocialScience':
+            $oneHighlight = 'wip';
+            break;
+        case 'Mathematics':
+            $oneHighlight = 'wip';
+            break;
+        
+      }
+
       oci_bind_by_name($query, ':StudentID', $StudentID);
       oci_bind_by_name($query, ':CoreRequirements', $oneHighlight);
       // Execute the query
@@ -53,4 +111,3 @@ function insertWIPIntoDB($StudentID,$highlights)
 }
 
 ?>
-  
